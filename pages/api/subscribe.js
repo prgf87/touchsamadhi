77a0d18a969e152f5950fc7b110e3b4ec2256f7b
Pedro <50713178+prgf87@users.noreply.github.com>
2022-11-09 16:35:55 +0000

    mailchimp integrated

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,34 +1,49 @@
 export default function handler(req, res) {
   const { email } = req.body;
-
   console.log(`email: ${email}`);
 
-  const client = require('@mailchimp/mailchimp_marketing');
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
 
-  const API_KEY = process.env.MAILCHIMP_API_KEY;
-  const API_SERVER = process.env.MAILCHIMP_API_SERVER;
-  const LIST_ID = process.env.MAILCHIMP_AUDIENCE_ID;
+  try {
+    const client = require('@mailchimp/mailchimp_marketing');
 
-  client.setConfig({
-    apiKey: API_KEY,
-    server: API_SERVER,
-  });
+    const API_KEY = process.env.MAILCHIMP_API_KEY;
+    const API_SERVER = process.env.MAILCHIMP_API_SERVER;
+    const LIST_ID = process.env.MAILCHIMP_AUDIENCE_ID;
 
-  const run = async () => {
-    const ping = await client.ping.get();
-    console.log(ping);
-    const response = await client.lists.setListMember(
-      LIST_ID,
-      'subscriber_hash',
-      {
-        email_address: email,
-        status_if_new: 'pending',
-        status: 'subscribed',
-        skip_merge_validation: true,
+    client.setConfig({
+      apiKey: API_KEY,
+      server: API_SERVER,
+    });
+
+    const run = async () => {
+      const ping = await client.ping.get();
+      console.log(ping);
+      const response = await client.lists.setListMember(
+        LIST_ID,
+        'subscriber_hash',
+        {
+          email_address: email,
+          status_if_new: 'subscribed',
+          status: 'subscribed',
+          skip_merge_validation: true,
+        }
+      );
+      console.log(response);
+      if (response.status >= 400) {
+        return res.status(400).json({
+          error: `There was an error subscribing your email address to the newsletter.
+          Please contact us directly at touchsamadhi@gmail.com so we can add you in manually. Sorry for any inconvenience.`,
+        });
       }
-    );
-    console.log(response);
-  };
-
-  run();
+    };
+    run();
+    return res
+      .status(201)
+      .json({ error: 'Email address has been added to the mailing list' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
 }
--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -19,7 +19,6 @@ export default function Contact() {
 
     const { error } = await res.json();
 
-    // 4. If there was an error, update the message in state.
     if (error) {
       setMessage(error);
 
@@ -30,16 +29,14 @@ export default function Contact() {
   };
 
   return (
-    <div className="h-[175px] bg-gray-100/70 text-black/70">
+    <div className="h-[200px] bg-gray-100/70 text-black/70">
       <div className="flex justify-center items-center pt-2">
-        <h1 className="font-bold text-center text-sm md:text-2xl">
+        <h1 className="font-bold text-center text-lg md:text-2xl">
           Sign up to the Newsletter...
         </h1>
       </div>
       <div className="text-center text-sm pb-2">
-        {message
-          ? message
-          : `We only send emails when we have genuine news. No spam.`}
+        {message ? message : `We only send emails when we have genuine news.`}
       </div>
       <div className="flex justify-center items-center w-full">
         <form onSubmit={subscribe}>
@@ -52,14 +49,14 @@ export default function Contact() {
             required
             autoCapitalize="off"
             autoCorrect="off"
-            className="border border-black/60 rounded-xl min-w-[300px] py-2 text-center px-2 mx-20"
+            className="border border-black/60 rounded-xl min-w-[280px] md:min-w-[400px] py-2 text-center text-md px-2"
           />
           <div className="flex justify-center items-center">
             <button
               type="submit"
               value=""
               name="subscribe"
-              className="px-4 py-1 mt-2 bg-gray-200 border border-black/60 text-gray-900 text-lg transition-colors duration-700 transform hover:bg-gray-500 hover:text-gray-100"
+              className="px-12 py-1 mt-2 text-lg border border-black/60 rounded-xl bg-gray-200 text-gray-900 transition-colors duration-700 transform hover:bg-gray-500 hover:text-gray-100"
             >
               Subscribe
             </button>
