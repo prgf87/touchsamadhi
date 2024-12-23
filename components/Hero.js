08c5211164cabf1971600210d82eeea32d8f1ba0
Pedro <prgf2011@gmail.com>
2024-12-23 17:49:09 +0000

    updates nextjs vunerability and adds slide animation to hero image

--- components/Hero.js
+++ components/Hero.js
@@ -1,46 +1,78 @@
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import Image from "next/legacy/image";
-import { slides } from "../public/lib/Slides";
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/legacy/image';
+import { slides } from '../public/lib/Slides';
 
 const Hero = () => {
+  const [slideDirection, setSlideDirection] = useState('right');
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (current === slides.length - 1) {
-        setCurrent(0);
-      } else {
-        setCurrent(current + 1);
-      }
-    }, 10000);
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 7000);
 
-    return () => clearInterval(intervalId);
-  }, [current]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [current, nextSlide]);
+
+  function getPositionClass(index) {
+    if (index === current) return 'translate-x-0';
+    if (slideDirection === 'right') {
+      if (index === getPrevIndex()) return '-translate-x-full';
+      return 'translate-x-full';
+    } else {
+      if (index === getNextIndex()) return 'translate-x-full';
+      return '-translate-x-full';
+    }
+  }
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  var nextSlide = () => {
+    setSlideDirection('right');
+    setCurrent(getNextIndex());
+  };
+
+  const getPrevIndex = () => (current === 0 ? slides.length - 1 : current - 1);
+  const getNextIndex = () => (current === slides.length - 1 ? 0 : current + 1);
 
   return (
-    <div className="relative flex justify-center items-top text-center min-h-[48vh] bg-center body">
-      <Image
-        src={slides[current].image}
-        objectPosition={slides[current].position}
-        alt="/"
-        placeholder="blur"
-        blurDataURL={slides[current]}
-        layout={"fill"}
-        objectFit={"cover"}
-        priority="true"
-      />
-
-      <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-black/80 z-[2]" />
-
-      {/* <div className="absolute bottom-[5%] w-full flex justify-center items-center ">
-        <Link href={'https://www.eqfest.com'}>
-          <button className="button btn px-12 py-2 text-2xl z-[2]">
-            Equinox
-          </button>
-        </Link>
-      </div> */}
-    </div>
+    <>
+      <div
+        id="gallery"
+        className="relative z-0 mb-4 max-w-full overflow-hidden  pt-4 sm:mx-auto md:pt-6  lg:pt-12"
+      >
+        <div className="relative flex justify-center items-top text-center min-h-[48vh] bg-center body">
+          <div className="absolute inset-0 overflow-hidden">
+            {[getPrevIndex(), current, getNextIndex()].map((index) => (
+              <div
+                key={index}
+                className={`absolute inset-0 transition-transform duration-1000 ease-in-out ${getPositionClass(
+                  index
+                )}`}
+              >
+                <Image
+                  src={slides[index].image}
+                  objectPosition={slides[current].position}
+                  alt={`slider image - ${slides[index].image}`}
+                  placeholder="blur"
+                  blurDataURL={slides[current]}
+                  layout={'fill'}
+                  objectFit={'cover'}
+                  priority="true"
+                  className={`w-full object-cover bg-center`}
+                />
+                <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-black/75 z-[2]" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
